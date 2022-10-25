f510f460e5d6d0e1201dfceedd7fffcdcb841e58
Gonzalo <4588601+z4gon@users.noreply.github.com>
2022-10-25 18:28:15 -0300

    refactor bubble

--- Assets/Scripts/BubblesComputeShaderLink.cs
+++ Assets/Scripts/BubblesComputeShaderLink.cs
@@ -1,85 +1,37 @@
 using UnityEngine;
 
-namespace Bubbles
+public class BubblesComputeShaderLink : BaseComputeShaderLink
 {
-    public enum KernelName
+    private const int THREAD_GROUP_COUNT = 1;
+
+    public Vector3 CirclePositionAndRadius = new Vector3(0, 0, 64);
+    public Color CircleColor = new Color(1, 0, 0, 1);
+
+    protected override void Start()
     {
-        DrawCircle,
+        base.Start();
+        DispatchShader(_kernelIndexes[0], THREAD_GROUP_COUNT, THREAD_GROUP_COUNT);
     }
 
-    public class BubblesComputeShaderLink : MonoBehaviour
+    protected override void InitShader()
     {
-        private const int TEXTURE_RESOLUTION = 256;
-        private const int THREAD_GROUP_COUNT = 1;
+        base.InitShader();
 
-        public ComputeShader computeShader;
-        public KernelName KernelName = KernelName.DrawCircle;
+        computeShader.SetVector("CirclePositionAndRadius", CirclePositionAndRadius);
+        computeShader.SetVector("CircleColor", CircleColor);
 
-        // our compute shader has numthreads(8,8,1) per each thread group
-        // 32 thread groups means 32 * 8 = 256 pixels, that covers the whole texture
-
-        public Vector3 CirclePositionAndRadius = new Vector3(0, 0, 64);
-        public Color CircleColor = new Color(1, 0, 0, 1);
-
-        private Renderer _renderer;
-        private RenderTexture _renderTexture;
-
-        private int _kernelIndex;
-        private bool _isInitialized = false;
-
-        void Start()
-        {
-            // initialize the texture with just x and y dimensions, it could have z depth
-            _renderTexture = new RenderTexture(TEXTURE_RESOLUTION, TEXTURE_RESOLUTION, 0);
-
-            // allow the compute shader to write to the texture
-            _renderTexture.enableRandomWrite = true;
-
-            // RenderTexture constructor does not actually create the hardware texture
-            // https://docs.unity3d.com/ScriptReference/RenderTexture.Create.html
-            _renderTexture.Create();
-
-            _renderer = GetComponent<Renderer>();
-            _renderer.enabled = true;
-
-            // initialize the shader
-            InitShader();
-
-            DispatchShader(THREAD_GROUP_COUNT, THREAD_GROUP_COUNT);
-        }
-
-        private void InitShader()
-        {
-            // get a reference to the kernel defined in the #pragma inside the compute shader
-            _kernelIndex = computeShader.FindKernel(KernelName.ToString());
-
-            // set the texture to the compute shader, so it can write to it
-            computeShader.SetTexture(_kernelIndex, "Result", _renderTexture);
-            computeShader.SetInt("TextureResolution", TEXTURE_RESOLUTION);
-            computeShader.SetVector("CirclePositionAndRadius", CirclePositionAndRadius);
-            computeShader.SetVector("CircleColor", CircleColor);
-
-            // set the texture to the material, so it can use the texture
-            _renderer.material.SetTexture("_MainTex", _renderTexture);
-
-            _isInitialized = true;
-        }
-
-        // dispatches the kernel with the amount of thread groups = x * y * 1
-        // we keep z = 1 because we are working on a 2D texture, no need for depth
-        private void DispatchShader(int x, int y) => computeShader.Dispatch(_kernelIndex, x, y, 1);
-
-        public void OnValidate()
-        {
-            // Debug.Log($"TextureResolution: {TextureResolution}");
-            // Debug.Log($"ThreadGroupsX: {ThreadGroupsX}");
-            // Debug.Log($"ThreadGroupsY: {ThreadGroupsY}");
-
-            if (_isInitialized)
-            {
-                DispatchShader(THREAD_GROUP_COUNT, THREAD_GROUP_COUNT);
-            }
-        }
+        _isInitialized = true;
     }
 
+    public void OnValidate()
+    {
+        // Debug.Log($"TextureResolution: {TextureResolution}");
+        // Debug.Log($"ThreadGroupsX: {ThreadGroupsX}");
+        // Debug.Log($"ThreadGroupsY: {ThreadGroupsY}");
+
+        if (_isInitialized)
+        {
+            DispatchShader(_kernelIndexes[0], THREAD_GROUP_COUNT, THREAD_GROUP_COUNT);
+        }
+    }
 }
